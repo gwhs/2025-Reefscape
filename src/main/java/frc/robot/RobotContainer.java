971fdb5f5726b827a3f5469eb0e12f4aeccf5017
Caden <Cachen15@s.sfusd.edu>
2025-01-04 17:12:26 -0800

    hi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,14 +11,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-<<<<<<< HEAD
+
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsytems.CommandSwerveDrivetrain;
 import dev.doglog.DogLog;
 import dev.doglog.DogLogOptions;
-=======
->>>>>>> dev
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
