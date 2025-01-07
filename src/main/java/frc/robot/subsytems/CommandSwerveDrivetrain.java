cb77a9303aa8f424e2cd7accf50f77f62df72f44
Caden <Cachen15@s.sfusd.edu>
2025-01-06 18:40:50 -0800

    cleaned up code

--- src/main/java/frc/robot/subsytems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsytems/CommandSwerveDrivetrain.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
