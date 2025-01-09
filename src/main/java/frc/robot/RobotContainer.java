d310292e661326cd77a721401e43e2b376c73c81
pee <betan1@s.sfusd.edu>
2025-01-08 16:55:42 -0800

    test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -75,7 +75,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return Commands.none();
+    return autoChooser.getSelected();
   }
 
   private void configureAutonomous() {
