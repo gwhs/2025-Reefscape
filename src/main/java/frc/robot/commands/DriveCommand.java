d031b0c96ab8f8efb11da01496498739ac8fdd27
chvoong24 <chvoong@s.sfusd.edu>
2025-01-08 19:33:59 -0800

    we did the task that we were assigned today which was make the robot face the source

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -17,46 +17,73 @@ import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class DriveCommand extends Command {
-  private double MaxSpeed =
-      TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12VoltsMps desired top speed
-  private double MaxAngularRate =
-      3.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+  private static final double PID_MAX = 0.35;
 
-  public static final double PID_MAX = 0.35;
-  public boolean isBackCoralStation = false;
 
-  private PIDController PID;
-  private CommandSwerveDrivetrain drivetrain;
-  private CommandXboxController driverController;
+  private final CommandSwerveDrivetrain drivetrain;
+  private final CommandXboxController driverController;
+  private final PIDController PID;
   private Pose2d currPose;
+
+
+  public boolean isBackCoralStation = false;
   public boolean isSlow = false;
-  private final SwerveRequest.FieldCentric drive =
+
+
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+  private double MaxAngularRate = 3.5 * Math.PI;
+
+
+  private static final double halfWidthField = 4.0359;
+  private static final double blueLeftCoralTargetX = 1.27;
+  private static final double blueLeftCoralTargetY = 1.27;
+  private static final double blueRightCoralTargetX = 1.27;
+  private static final double blueRightCoralTargetY = 6.80;
+  private static final double redLeftCoralTargetX = 16.5;
+  private static final double redLeftCoralTargetY = 1.27;
+  private static final double redRightCoralTargetX = 16.5;
+  private static final double redRightCoralTargetY = 6.80;
+
+
+  private final SwerveRequest.FieldCentric drive = 
       new SwerveRequest.FieldCentric()
           .withDeadband(MaxSpeed * 0.1)
-          .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+          .withRotationalDeadband(MaxAngularRate * 0.1)
+          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-  // driving in open loop
 
   public DriveCommand(CommandXboxController driverController, CommandSwerveDrivetrain drivetrain) {
-    // Use addRequirements() here to declare subsystem dependencies.
     this.driverController = driverController;
     this.drivetrain = drivetrain;
 
+
     this.PID = new PIDController(0.02, 0, 0);
     this.PID.setTolerance(0.1);
+  
     this.PID.enableContinuousInput(-180, 180);
+
 
     addRequirements(drivetrain);
   }
 
+
+  @Override
+  public void initialize() {
+
+    isBackCoralStation = true;
+  }
+
+
+  @Override
   public void execute() {
     double xVelocity = -driverController.getLeftY();
     double yVelocity = -driverController.getLeftX();
     double angularVelocity = -driverController.getRightX();
 
+
     currPose = drivetrain.getState().Pose;
     double currTheta = currPose.getRotation().getDegrees();
+
 
     if (isSlow) {
       double slowFactor = 0.25;
@@ -64,78 +91,107 @@ public class DriveCommand extends Command {
       yVelocity *= slowFactor;
       angularVelocity *= slowFactor;
     }
-    
+
+
+  
     if (isBackCoralStation) {
-      double ang = backCoralTheta(currPose);
-      PID.setSetpoint(ang);
-      angularVelocity = MathUtil.clamp(PID.calculate(currTheta), -PID_MAX, PID_MAX);
-      SmartDashboard.putNumber("isBackSpeaker Goal", ang);
-      SmartDashboard.putNumber("isBackSpeaker Result", angularVelocity);
+
+      double coralX, coralY;
+      if (DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
+        if (currPose.getY() <= halfWidthField) {
+          coralX = blueLeftCoralTargetX;
+          coralY = blueLeftCoralTargetY;
+        } else {
+          coralX = blueRightCoralTargetX;
+          coralY = blueRightCoralTargetY;
+        }
+      } else {
+        // Red or invalid
+        if (currPose.getY() <= halfWidthField) {
+          coralX = redLeftCoralTargetX;
+          coralY = redLeftCoralTargetY;
+        } else {
+          coralX = redRightCoralTargetX;
+          coralY = redRightCoralTargetY;
+        }
       }
 
-    xVelocity = xVelocity * MaxSpeed;
-    yVelocity = yVelocity * MaxSpeed;
-    angularVelocity = angularVelocity * MaxAngularRate;
+
+      double desiredAngle = calculateBackAngleToTarget(currPose, coralX, coralY);
+      PID.setSetpoint(desiredAngle);
+
+
+      double pidOutput = PID.calculate(currTheta);
+      pidOutput = MathUtil.clamp(pidOutput, -PID_MAX, PID_MAX);
+
+
+      angularVelocity = pidOutput;
+
+
+      SmartDashboard.putNumber("CoralTrackingDesiredAngle", desiredAngle);
+      SmartDashboard.putNumber("CoralTrackingPIDOutput", pidOutput);
+    }
+
+
+
+    xVelocity *= MaxSpeed;
+    yVelocity *= MaxSpeed;
+    angularVelocity *= MaxAngularRate;
+
+
 
     DogLog.log("Drive Command/xVelocity", xVelocity);
     DogLog.log("Drive Command/yVelocity", yVelocity);
     DogLog.log("Drive Command/angularVelocity", angularVelocity);
 
+
+
     drivetrain.setControl(
         drive
-            .withVelocityX(xVelocity) // Drive forward with negative Y (forward)
-            .withVelocityY(yVelocity) // Drive left with negative X (left)
-            .withRotationalRate(angularVelocity)); // Drive counterclockwise with negative X (left)
-  }
-  public static double caclucateRotateTheta(Pose2d pose, double targetX, double targetY){
-    double calucatedRad = Math.atan((targetY-pose.getY())/ (targetX-pose.getX()));
-    
-    return  Math.abs(Math.toDegrees(calucatedRad));
-  }
-  public double backCoralTheta(Pose2d pose)
-  {
-    double halfWidthField = 4.0359;
-    double leftCoralTargetX = 1.27;
-    double leftCoralTargetY = 6.80;
-    double rightCoralTargetX = 1.27;
-    double rightCoralTargetY = 1.27;
-    
-      if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue)
-      {
-           if(pose.getY() >= halfWidthField)
-      {
-          return caclucateRotateTheta(pose, leftCoralTargetX, leftCoralTargetY);
-      }
-      else 
-      {
-          return caclucateRotateTheta(pose, rightCoralTargetX, rightCoralTargetY);
-      }
-      }
-      else
-      {
-        if(pose.getY() >= halfWidthField)
-        {
-            return caclucateRotateTheta(pose, rightCoralTargetX, rightCoralTargetY);
-        }
-        else 
-        {
-          return caclucateRotateTheta(pose, leftCoralTargetX, leftCoralTargetY);
-        }
-      }
-    }
-
-  @Override
-  public void end(boolean interrupted) {}
-
-  @Override
-  public void initialize()
-  {
-    isBackCoralStation=!isBackCoralStation;
+            .withVelocityX(xVelocity)
+            .withVelocityY(yVelocity)
+            .withRotationalRate(angularVelocity)
+    );
   }
 
-  // Returns true when the command should end.
+
   @Override
   public boolean isFinished() {
     return false;
   }
+
+
+  public static double calculateBackAngleToTarget(Pose2d pose, double targetX, double targetY) {
+    double dx = targetX - pose.getX();
+    double dy = targetY - pose.getY();
+
+
+    double angleToTargetRad = Math.atan2(dy, dx);  // angle from -π to π
+    double angleToTargetDeg = Math.toDegrees(angleToTargetRad); // -180 to 180
+
+
+    double backAngle = angleToTargetDeg + 180;
+
+
+    return wrapDegrees(backAngle);
+  }
+
+
+  /**
+   * Wrap angle to [-180, 180].
+   */
+  public static double wrapDegrees(double angleDeg) {
+    double wrapped = angleDeg % 360.0;
+    if (wrapped > 180.0) {
+      wrapped -= 360.0;
+    } else if (wrapped <= -180.0) {
+      wrapped += 360.0;
+    }
+    return wrapped;
+  }
 }
+
+
+
+
+
