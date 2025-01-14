5bd9f771145fa07a8c095f05bc0e455261257cf5
Emma Kadlecek <kadlecekemma@gmail.com>
2025-01-13 19:14:58 -0800

    added some magic

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -4,12 +4,13 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import dev.doglog.DogLog;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import dev.doglog.DogLog;
+
 public class DriveCommand extends Command {
 
   private double MaxSpeed =
