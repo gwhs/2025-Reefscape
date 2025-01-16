ad7c3ef9bc756b219855ef004a2893fa930cd82e
Morgan Jiang <morganjiang9768@Gmail.com>
2025-01-15 17:28:11 -0800

    fixed more jons

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -5,10 +5,7 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import dev.doglog.DogLog;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.generated.TunerConstants;
@@ -16,35 +13,23 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class DriveCommand extends Command {
 
+  private double maxSpeed =
+      TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12VoltsMps desired top speed
+  private double maxAngularRate =
+      3.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+
   public static final double PID_MAX = 0.35;
+
+  private PIDController PID;
+
+  private CommandSwerveDrivetrain drivetrain;
+  private CommandXboxController driverController;
 
   public boolean isSlow = true;
   public boolean robotCentric = false;
 
   private final SwerveRequest.FieldCentric fieldCentricDrive =
-  private final CommandSwerveDrivetrain drivetrain;
-  private final CommandXboxController driverController;
-  private final PIDController PID;
-  private Pose2d currPose;
-
-  public boolean isBackCoralStation = false;
-
-  private double maxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-  private double maxAngularRate = 3.5 * Math.PI;
-
-  private final double RED_LEFT_STATION_ANGLE = 126;
-  private final double RED_RIGHT_STATION_ANGLE = -126;
-  private final double BLUE_LEFT_STATION_ANGLE = 54;
-  private final double BLUE_RIGHT_STATION_ANGLE = -54;
-
-  // Unit is meters
-  private static final double halfWidthField = 4.0359;
-
-  private final SwerveRequest.FieldCentric drive =
       new SwerveRequest.FieldCentric()
-          .withDeadband(maxSpeed * 0.1)
-          .withRotationalDeadband(maxAngularRate * 0.1)
-          .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
           .withDeadband(maxSpeed * 0.1)
           .withRotationalDeadband(maxAngularRate * 0.1) // Add a 10% deadband
           .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
@@ -68,7 +53,6 @@ public class DriveCommand extends Command {
   }
 
   @Override
-  @Override
   public void execute() {
     double xVelocity = -driverController.getLeftY();
     double yVelocity = -driverController.getLeftX();
@@ -82,40 +66,9 @@ public class DriveCommand extends Command {
       angularVelocity *= slowFactor;
     }
 
-    if (isBackCoralStation) {
-      if (DriverStation.getAlliance().isPresent()
-          && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
-        // Blue Alliance
-        if (currPose.getY() <= halfWidthField) {
-          // Low Y => "Right" station for Blue
-          PID.setSetpoint(BLUE_LEFT_STATION_ANGLE);
-        } else {
-          // High Y => "Left" station for Blue
-          PID.setSetpoint(BLUE_RIGHT_STATION_ANGLE);
-        }
-      } else {
-        // Red Alliance or invalid
-        if (currPose.getY() <= halfWidthField) {
-          PID.setSetpoint(RED_LEFT_STATION_ANGLE);
-        } else {
-          PID.setSetpoint(RED_RIGHT_STATION_ANGLE);
-        }
-      }
-
-      // Feed the fixed angle into the PID
-      double pidOutput = PID.calculate(currTheta);
-      pidOutput = MathUtil.clamp(pidOutput, -PID_MAX, PID_MAX);
-
-      // Override the user's rotation with the PID result
-      angularVelocity = pidOutput;
-
-      DogLog.log("Drive Command/CoralTrackingPIDOutput", pidOutput);
-    }
-
-    // Multiply by our maximum speeds/rates
-    xVelocity *= maxSpeed;
-    yVelocity *= maxSpeed;
-    angularVelocity *= maxAngularRate;
+    xVelocity = xVelocity * maxSpeed;
+    yVelocity = yVelocity * maxSpeed;
+    angularVelocity = angularVelocity * maxAngularRate;
 
     DogLog.log("Drive Command/xVelocity", xVelocity);
     DogLog.log("Drive Command/yVelocity", yVelocity);
