14289993eaedadcf59d6b5ef2055db77f01929ff
Caden <Cachen15@s.sfusd.edu>
2025-01-20 14:52:42 -0800

    made changes

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -6,24 +6,24 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
 public class EagleUtil {
-  private static double blue_reef_x = Units.inchesToMeters(144 + (93.5 - 14 * 2) / 2);
-  private static double blue_reef_y = Units.inchesToMeters(158.50);
-  private static Pose2d blue_reef = new Pose2d(blue_reef_x, blue_reef_y, Rotation2d.kZero);
-  private static Pose2d blue_reef_invert = new Pose2d(-blue_reef_x, -blue_reef_y, Rotation2d.kZero);
+  private static double BLUE_REEF_X = Units.inchesToMeters(144 + (93.5 - 14 * 2) / 2);
+  private static double BLUE_REEF_Y = Units.inchesToMeters(158.50);
+  private static Pose2d BLUE_REEF = new Pose2d(blue_reef_x, blue_reef_y, Rotation2d.kZero);
+  private static Pose2d BLUE_REEF_INVERT = new Pose2d(-blue_reef_x, -blue_reef_y, Rotation2d.kZero);
 
-  private static double red_reef_x = Units.inchesToMeters(546.875 - (93.5 - 14 * 2) / 2);
-  private static double red_reef_y = Units.inchesToMeters(158.50);
-  private static Pose2d red_reef = new Pose2d(red_reef_x, red_reef_y, Rotation2d.kZero);
-  private static Pose2d red_reef_invert = new Pose2d(-red_reef_x, -red_reef_y, Rotation2d.kZero);
+  private static double RED_REEF_X = Units.inchesToMeters(546.875 - (93.5 - 14 * 2) / 2);
+  private static double RED_REEF_Y = Units.inchesToMeters(158.50);
+  private static Pose2d RED_REEF = new Pose2d(red_reef_x, red_reef_y, Rotation2d.kZero);
+  private static Pose2d RED_REEF_INVERT = new Pose2d(-red_reef_x, -red_reef_y, Rotation2d.kZero);
 
-  private static double reef_length = (2.37 / 2) * Math.tanh(Units.degreesToRadians(30));
+  private static double REEF_LENGTH = (2.37 / 2) * Math.tanh(Units.degreesToRadians(30));
 
-  private static double reef_to_reef_distance = 0.33;
+  private static double REEF_TO_REEF_DISTANCE = 0.33;
 
-  private static double robot_away_from_reef = Units.inchesToMeters(50) / 2;
+  private static double ROBOT_AWAY_FROM_REEF = Units.inchesToMeters(50) / 2;
 
-  private static double x = -reef_length - robot_away_from_reef;
-  private static double y = reef_to_reef_distance / 2;
+  private static double X = -reef_length - robot_away_from_reef;
+  private static double Y = reef_to_reef_distance / 2;
 
   // blue poses
   private static Pose2d[] bluePoses = new Pose2d[12];
