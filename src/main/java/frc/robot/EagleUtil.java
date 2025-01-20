0137ba7075ab7be860cc07e1347a079e7cdccc23
Caden <Cachen15@s.sfusd.edu>
2025-01-20 14:55:16 -0800

    made changes to the changes

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -8,13 +8,13 @@ import edu.wpi.first.math.util.Units;
 public class EagleUtil {
   private static double BLUE_REEF_X = Units.inchesToMeters(144 + (93.5 - 14 * 2) / 2);
   private static double BLUE_REEF_Y = Units.inchesToMeters(158.50);
-  private static Pose2d BLUE_REEF = new Pose2d(blue_reef_x, blue_reef_y, Rotation2d.kZero);
-  private static Pose2d BLUE_REEF_INVERT = new Pose2d(-blue_reef_x, -blue_reef_y, Rotation2d.kZero);
+  private static Pose2d BLUE_REEF = new Pose2d(BLUE_REEF_X, BLUE_REEF_Y, Rotation2d.kZero);
+  private static Pose2d BLUE_REEF_INVERT = new Pose2d(-BLUE_REEF_X, -BLUE_REEF_Y, Rotation2d.kZero);
 
   private static double RED_REEF_X = Units.inchesToMeters(546.875 - (93.5 - 14 * 2) / 2);
   private static double RED_REEF_Y = Units.inchesToMeters(158.50);
-  private static Pose2d RED_REEF = new Pose2d(red_reef_x, red_reef_y, Rotation2d.kZero);
-  private static Pose2d RED_REEF_INVERT = new Pose2d(-red_reef_x, -red_reef_y, Rotation2d.kZero);
+  private static Pose2d RED_REEF = new Pose2d(RED_REEF_X, RED_REEF_Y, Rotation2d.kZero);
+  private static Pose2d RED_REEF_INVERT = new Pose2d(-RED_REEF_X, -RED_REEF_Y, Rotation2d.kZero);
 
   private static double REEF_LENGTH = (2.37 / 2) * Math.tanh(Units.degreesToRadians(30));
 
@@ -22,8 +22,8 @@ public class EagleUtil {
 
   private static double ROBOT_AWAY_FROM_REEF = Units.inchesToMeters(50) / 2;
 
-  private static double X = -reef_length - robot_away_from_reef;
-  private static double Y = reef_to_reef_distance / 2;
+  private static double X = -REEF_LENGTH - ROBOT_AWAY_FROM_REEF;
+  private static double Y = REEF_TO_REEF_DISTANCE / 2;
 
   // blue poses
   private static Pose2d[] bluePoses = new Pose2d[12];
@@ -36,8 +36,8 @@ public class EagleUtil {
     for (int i = 0; i < bluePoses.length; i++) {
       System.out.print(bluePoses[i] + ", ");
     }
-    bluePoses[0] = new Pose2d(x, y, Rotation2d.kZero);
-    bluePoses[1] = new Pose2d(x, -y, Rotation2d.kZero);
+    bluePoses[0] = new Pose2d(X, Y, Rotation2d.kZero);
+    bluePoses[1] = new Pose2d(X, -Y, Rotation2d.kZero);
 
     Rotation2d sixty = Rotation2d.fromDegrees(60);
 
@@ -46,10 +46,10 @@ public class EagleUtil {
     }
 
     for (int i = 0; i < bluePoses.length; i++) {
-      bluePoses[i] = bluePoses[i].relativeTo(blue_reef_invert);
+      bluePoses[i] = bluePoses[i].relativeTo(BLUE_REEF_INVERT);
     }
 
-    DogLog.log("Blue Reef", blue_reef);
+    DogLog.log("Blue Reef", BLUE_REEF);
     DogLog.log("Blue Set Points", bluePoses);
   }
 
@@ -57,8 +57,8 @@ public class EagleUtil {
     for (int i = 0; i < redPoses.length; i++) {
       System.out.print(redPoses[i] + ", ");
     }
-    redPoses[0] = new Pose2d(x, y, Rotation2d.kZero);
-    redPoses[1] = new Pose2d(x, -y, Rotation2d.kZero);
+    redPoses[0] = new Pose2d(X, Y, Rotation2d.kZero);
+    redPoses[1] = new Pose2d(X, -Y, Rotation2d.kZero);
 
     Rotation2d sixty = Rotation2d.fromDegrees(60);
 
@@ -67,9 +67,9 @@ public class EagleUtil {
     }
 
     for (int i = 0; i < redPoses.length; i++) {
-      redPoses[i] = redPoses[i].relativeTo(red_reef_invert);
+      redPoses[i] = redPoses[i].relativeTo(RED_REEF_INVERT);
     }
-    DogLog.log("Red Reef", red_reef);
+    DogLog.log("Red Reef", RED_REEF);
     DogLog.log("Red Set Points", redPoses);
   }
 }
