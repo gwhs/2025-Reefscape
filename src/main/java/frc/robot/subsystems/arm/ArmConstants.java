6e54e440c41ad09fa0f6ed8fb496e89b059c1100
Morgan Jiang <morganjiang9768@Gmail.com>
2025-01-22 15:34:38 -0800

    changed id

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.Arm;
 
 public class ArmConstants {
-  public static final int ARM_MOTOR_ID = 1;
+  public static final int ARM_MOTOR_ID = 54;
   public static final double MAX_VELOCITY = 300;
   public static final double MAX_ACCELERATION = 150;
   public static final double ARM_GEAR_RATIO = 62.3333333333;
