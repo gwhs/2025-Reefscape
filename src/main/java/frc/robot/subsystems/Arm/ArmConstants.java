2e48f1dfeced129f7f1dd28ba40a7328f7fe9e99
Isaac Ng <isaacngkh@gmail.com>
2025-01-25 11:23:31 -0800

    Delete src/main/java/frc/robot/subsystems/Arm directory

--- src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.arm;
-
-public class ArmConstants {
-  public static final int ARM_MOTOR_ID = 54;
-  public static final double MAX_VELOCITY = 300;
-  public static final double MAX_ACCELERATION = 150;
-  public static final double ARM_GEAR_RATIO = 62.3333333333;
-}
--- src/main/java/frc/robot/subsystems/Arm/ArmIO.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.arm;
-
-public interface ArmIO {
-  public void setAngle(double angle);
-
-  public double getPosition();
-
-  public void update();
-}
