126382ba5bfc6dc6e61e7ee62cba2744a5f92dd5
Emma Kadlecek <kadlecekemma@gmail.com>
2025-01-27 17:45:50 -0800

    grumble grumble

--- src/main/java/frc/robot/subsystems/led/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LedSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.led;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -11,20 +12,30 @@ public class LedSubsystem extends SubsystemBase {
   AddressableLEDBuffer m_LedBuffer;
 
   public LedSubsystem() {
-    m_led = new AddressableLED(LedUtils.PWM_LED_PORT);
-    m_LedBuffer = new AddressableLEDBuffer(LedUtils.LED_LENGTH);
+    if (RobotBase.isSimulation()) {
+      return;
+    } else {
+      m_led = new AddressableLED(LedUtils.PWM_LED_PORT);
+      m_LedBuffer = new AddressableLEDBuffer(LedUtils.LED_LENGTH);
 
-    m_led.setLength(m_LedBuffer.getLength());
-    m_led.start();
+      m_led.setLength(m_LedBuffer.getLength());
+      m_led.start();
+    }
   }
 
   public void setColor(LEDPattern pattern) {
+    if (RobotBase.isSimulation()) {
+      return;
+    }
+
     pattern.applyTo(m_LedBuffer);
     m_led.setData(m_LedBuffer);
   }
 
   public Color getColor(int index) {
-
+    if (RobotBase.isSimulation()) {
+      return Color.kBlack;
+    }
     return m_LedBuffer.getLED(index);
   }
 
