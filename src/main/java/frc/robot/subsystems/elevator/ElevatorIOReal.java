1fdb449a3d16030701d5d7a9368be726738382d0
Caden <Cachen15@s.sfusd.edu>
2025-01-29 14:35:50 -0800

    KG

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
@@ -41,6 +41,7 @@ public class ElevatorIOReal implements ElevatorIO {
     SoftwareLimitSwitchConfigs softwareLimitSwitchConfigs = talonFXConfigs.SoftwareLimitSwitch;
 
     slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+    slot0Configs.kG = 0; // Add 0 voltage to overcome gravity
     slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
     slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
     slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
