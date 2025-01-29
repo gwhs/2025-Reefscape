e0e61f1f65d2c75e2e282cba7f59932ec4d196c1
isaacngkh <isaacngkh@gmail.com>
2025-01-29 14:01:11 -0800

    added back led subsystem

---
+++ src/main/java/frc/robot/subsystems/led/LedSubsystem.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems.led;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LedSubsystem extends SubsystemBase {
+  AddressableLED m_led;
+  AddressableLEDBuffer m_LedBuffer;
+
+  public LedSubsystem() {
+    if (RobotBase.isSimulation()) {
+      return;
+    }
+    m_led = new AddressableLED(LedConstants.PWM_LED_PORT);
+    m_LedBuffer = new AddressableLEDBuffer(LedConstants.LED_LENGTH);
+
+    m_led.setLength(m_LedBuffer.getLength());
+    m_led.start();
+  }
+
+  public void setColor(LEDPattern pattern) {
+    if (RobotBase.isSimulation()) {
+      return;
+    }
+    pattern.applyTo(m_LedBuffer);
+    m_led.setData(m_LedBuffer);
+  }
+
+  public Color getColor(int index) {
+    if (RobotBase.isSimulation()) {
+      return Color.kBlack;
+    }
+    return m_LedBuffer.getLED(index);
+  }
+
+  public void turnOff() {
+    setColor(LEDPattern.solid(Color.kBlack));
+  }
+}
