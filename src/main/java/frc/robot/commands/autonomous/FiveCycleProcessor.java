bfd1da22207f3ec5401ca23a2248d0c8bc099310
Isaac Ng <isaacngkh@gmail.com>
2025-02-02 19:43:20 -0800

    cleanup

--- src/main/java/frc/robot/commands/autonomous/FiveCycleProcessor.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycleProcessor.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
 
 public class FiveCycleProcessor extends PathPlannerAuto {
   public FiveCycleProcessor(RobotContainer robotContainer, boolean partTwoOnly) {
@@ -32,36 +33,47 @@ public class FiveCycleProcessor extends PathPlannerAuto {
       double waitTime = 0.05;
       double scoringTime = 0.2;
 
+      Pose2d startingPosePart1 =
+          new Pose2d(SC_F.getPoint(0).position, SC_F.getIdealStartingState().rotation());
+      Pose2d startingPosePart2 =
+          new Pose2d(F_CSP.getPoint(0).position, F_CSP.getIdealStartingState().rotation());
+
+      Trigger partTwoOnlyTrigger = new Trigger(() -> partTwoOnly);
+
+      /* Part 1 beginning: Starting line to F, score preload, go to coral station */
       isRunning()
-          .and(new Trigger(() -> !partTwoOnly))
+          .and(partTwoOnlyTrigger.negate())
           .onTrue(
               Commands.sequence(
-                      AutoBuilder.resetOdom(
-                          new Pose2d(
-                              SC_F.getPoint(0).position, SC_F.getIdealStartingState().rotation())),
+                      AutoBuilder.resetOdom(startingPosePart1),
                       AutoBuilder.followPath(SC_F),
-                      Commands.waitSeconds(scoringTime))
+                      robotContainer.scoreCoral().withTimeout(0.5),
+                      AutoBuilder.followPath(F_CSP).alongWith(robotContainer.prepCoralIntake()))
                   .withName("Leave SC to score preload at F"));
 
-      event("atF")
-          .or(isRunning().and(new Trigger(() -> partTwoOnly)))
+      /* Part 2 beginning: F to coral station */
+      isRunning()
+          .and(partTwoOnlyTrigger)
           .onTrue(
               Commands.sequence(
-                      AutoBuilder.resetOdom(
-                          new Pose2d(
-                              F_CSP.getPoint(0).position, SC_F.getIdealStartingState().rotation())),
-                      AutoBuilder.followPath(F_CSP))
+                      AutoBuilder.resetOdom(startingPosePart2),
+                      AutoBuilder.followPath(F_CSP).alongWith(robotContainer.prepCoralIntake()))
                   .withName("F to CSP"));
 
+      /* At Coral Station, intake and go to E, score, E to coral station */
       event("atCSP_F")
           .onTrue(
               Commands.sequence(
                       Commands.waitSeconds(waitTime),
-                      AutoBuilder.followPath(CSP_E),
-                      Commands.waitSeconds(scoringTime),
-                      AutoBuilder.followPath(E_CSP))
+                      AutoBuilder.followPath(CSP_E)
+                          .alongWith(
+                              robotContainer.prepScoreCoral(
+                                  ElevatorSubsystem.rotationsToMeters(57), 210)),
+                      robotContainer.scoreCoral().withTimeout(0.5),
+                      AutoBuilder.followPath(E_CSP).alongWith(robotContainer.prepCoralIntake()))
                   .withName("CSP to E"));
 
+      /* At Coral Station, intake and go to D, score, D to coral station */
       event("atCSP_E")
           .onTrue(
               Commands.sequence(
@@ -71,6 +83,7 @@ public class FiveCycleProcessor extends PathPlannerAuto {
                       AutoBuilder.followPath(D_CSP))
                   .withName("CSP to D"));
 
+      /* At Coral Station, intake and go to C, score, C to coral station */
       event("atCSP_D")
           .onTrue(
               Commands.sequence(
@@ -80,6 +93,7 @@ public class FiveCycleProcessor extends PathPlannerAuto {
                       AutoBuilder.followPath(C_CSP))
                   .withName("CSP to C"));
 
+      /* At Coral Station, intake and go to B, score, B to coral station */
       event("atCSP_C")
           .onTrue(
               Commands.sequence(
