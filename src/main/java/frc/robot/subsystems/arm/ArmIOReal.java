44e76422c261eba37385b0b4d0c2925d6a5d5da8
Morgan Jiang <morganjiang9768@Gmail.com>
2025-02-05 16:17:19 -0800

    reapply configs until they succeed

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -1,12 +1,12 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -52,8 +52,14 @@ public class ArmIOReal implements ArmIO {
     currentConfig.withStatorCurrentLimitEnable(true);
     currentConfig.withStatorCurrentLimit(15);
 
-    TalonFXConfigurator leftElevatorConfigurator = armMotor.getConfigurator();
-    leftElevatorConfigurator.apply(talonFXConfigs);
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; i++) {
+      status = armMotor.getConfigurator().apply(talonFXConfigs);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not configure device. Error: " + status.toString());
+    }
 
     SmartDashboard.putData(
         "Arm Command/reset to 90",
