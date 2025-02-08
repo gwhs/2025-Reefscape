6892be923d21d484ac9161bb73ef214b2c4a1fac
R1shy <kadlecekemma@gmail.com>
2025-02-08 19:39:05 +0000

    added motor limits (#120)
    * added limits
    
    * .

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOTalon.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOTalon.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.endEffector;
 
 import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import dev.doglog.DogLog;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -14,6 +17,24 @@ class EndEffectorIOTalon implements EndEffectorIO {
   private final StatusSignal<Voltage> volts = motor.getMotorVoltage();
   private final StatusSignal<AngularVelocity> velocity = motor.getVelocity();
   private final StatusSignal<Temperature> temperature = motor.getDeviceTemp();
+
+  public EndEffectorIOTalon() {
+    TalonFXConfiguration talonConfig = new TalonFXConfiguration();
+    CurrentLimitsConfigs limitsConfigs = talonConfig.CurrentLimits;
+
+    limitsConfigs.withStatorCurrentLimitEnable(true);
+    limitsConfigs.withStatorCurrentLimit(15);
+
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+
+    for (int i = 0; i < 5; i++) {
+      status = motor.getConfigurator().apply(talonConfig);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not configure device. Error: " + status.toString());
+    }
+  }
 
   @Override
   public void setVoltage(double voltage) {
