0fe9b1d44e58d78b636d693c7d29378d82ed4c2e
belltan635 <belltan635@gmail.com>
2025-02-12 15:32:49 -0800

    moved elevator configs and changed left->front and right->back (#146)
    * added class differentialfollower and replaced all the follower ones
    
    * changed left->front, right->back
    
    * spotless
    
    * pushing
    
    * push
    
    * differential
    
    * hi
    
    * fixed statuses
    
    * fixed the back status thing oops
    
    * changed numbers
    
    ---------
    
    Co-authored-by: bella <betan1@s.sfusd.edu>
    Co-authored-by: chvoong24 <chvoong@s.sfusd.edu>
    Co-authored-by: R1shy <kadlecekemma@gmail.com>
    Co-authored-by: Caden <Cachen15@s.sfusd.edu>

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -8,13 +8,13 @@ import edu.wpi.first.math.util.Units;
 
 /** Add your docs here. */
 public class ElevatorConstants {
-  public static final int LEFT_ELEVATOR_MOTOR_ID = 21;
-  public static final int RIGHT_ELEVATOR_MOTOR_ID = 14;
+  public static final int FRONT_ELEVATOR_MOTOR_ID = 21;
+  public static final int BACK_ELEVATOR_MOTOR_ID = 14;
 
   public static final int GEAR_RATIO = 16;
   public static final double SPROCKET_DIAMETER = Units.inchesToMeters(1.7567);
 
-  public static final double TOP_METER = ElevatorSubsystem.rotationsToMeters(58);
+  public static final double TOP_METER = ElevatorSubsystem.rotationsToMeters(60 / 12 * 16);
 
   // Position constants:
   public static final double L3_PREP_POSITION = 0.4;
@@ -23,7 +23,7 @@ public class ElevatorConstants {
   public static final double L4_RELEASE_POSITION = 0.2;
 
   public static final double INTAKE_METER = ElevatorSubsystem.rotationsToMeters(0);
-  public static final double STOW_METER = ElevatorSubsystem.rotationsToMeters(15);
+  public static final double STOW_METER = ElevatorSubsystem.rotationsToMeters(15 / 12 * 16);
 
   public static final double MAX_VELOCITY = 80; // rps
   public static final double MAX_ACCELERATION = 160; // rps
