8167023ea3a034d74d82172afc0b0562c220e3bd
Caden <Cachen15@s.sfusd.edu>
2025-02-12 15:48:15 -0800

    changed from onTrue to whileTrue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -225,10 +225,10 @@ public class RobotContainer {
     // m_operatorController.a().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L2));
     // m_operatorController.x().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L1));
 
-    m_operatorController.y().onTrue(elevator.sysIdQuasistatic(Direction.kForward));
-    m_operatorController.b().onTrue(elevator.sysIdQuasistatic(Direction.kReverse));
-    m_operatorController.a().onTrue(elevator.sysIdDynamic(Direction.kForward));
-    m_operatorController.x().onTrue(elevator.sysIdDynamic(Direction.kReverse));
+    m_operatorController.y().whileTrue(elevator.sysIdQuasistatic(Direction.kForward));
+    m_operatorController.b().whileTrue(elevator.sysIdQuasistatic(Direction.kReverse));
+    m_operatorController.a().whileTrue(elevator.sysIdDynamic(Direction.kForward));
+    m_operatorController.x().whileTrue(elevator.sysIdDynamic(Direction.kReverse));
   }
 
   public void periodic() {
