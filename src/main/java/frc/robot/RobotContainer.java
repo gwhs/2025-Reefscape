051ec5cff9a4cb893857417a4e50b600f87143a7
wilsomc <wilsonchan115@gmail.com>
2025-02-12 17:06:44 -0800

    commented out bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -220,15 +220,15 @@ public class RobotContainer {
         .b()
         .whileTrue(alignToPose(() -> EagleUtil.closestReefSetPoint(drivetrain.getPose(), 1)));
 
-    // m_operatorController.y().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L4));
-    // m_operatorController.b().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L3));
-    // m_operatorController.a().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L2));
-    // m_operatorController.x().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L1));
+    m_operatorController.y().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L4));
+    m_operatorController.b().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L3));
+    m_operatorController.a().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L2));
+    m_operatorController.x().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L1));
 
-    m_operatorController.y().whileTrue(elevator.sysIdQuasistatic(Direction.kForward));
-    m_operatorController.b().whileTrue(elevator.sysIdQuasistatic(Direction.kReverse));
-    m_operatorController.a().whileTrue(elevator.sysIdDynamic(Direction.kForward));
-    m_operatorController.x().whileTrue(elevator.sysIdDynamic(Direction.kReverse));
+    // m_operatorController.y().whileTrue(elevator.sysIdQuasistatic(Direction.kForward));
+    // m_operatorController.b().whileTrue(elevator.sysIdQuasistatic(Direction.kReverse));
+    // m_operatorController.a().whileTrue(elevator.sysIdDynamic(Direction.kForward));
+    // m_operatorController.x().whileTrue(elevator.sysIdDynamic(Direction.kReverse));
   }
 
   public void periodic() {
