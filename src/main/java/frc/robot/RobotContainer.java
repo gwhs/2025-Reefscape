7cebd2dc40dd921bf902f03a023cd3f542039962
Morgan Jiang <morganjiang9768@Gmail.com>
2025-02-12 16:00:52 -0800

    changed commands to whiletrue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -215,20 +215,20 @@ public class RobotContainer {
     m_driverController
         .a()
         .whileTrue(alignToPose(() -> EagleUtil.getCachedReefPose(drivetrain.getState().Pose)));
-    
+
     m_driverController
         .b()
         .whileTrue(alignToPose(() -> EagleUtil.closestReefSetPoint(drivetrain.getPose(), 1)));
-    
+
     // m_operatorController.y().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L4));
     // m_operatorController.b().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L3));
     // m_operatorController.a().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L2));
     // m_operatorController.x().onTrue(Commands.runOnce(() -> coralLevel = CoralLevel.L1));
 
-    m_operatorController.y().onTrue(arm.sysIdQuasistatic(Direction.kForward));
-    m_operatorController.b().onTrue(arm.sysIdQuasistatic(Direction.kReverse));
-    m_operatorController.a().onTrue(arm.sysIdDynamic(Direction.kForward));
-    m_operatorController.x().onTrue(arm.sysIdDynamic(Direction.kReverse));
+    m_operatorController.y().whileTrue(arm.sysIdQuasistatic(Direction.kForward));
+    m_operatorController.b().whileTrue(arm.sysIdQuasistatic(Direction.kReverse));
+    m_operatorController.a().whileTrue(arm.sysIdDynamic(Direction.kForward));
+    m_operatorController.x().whileTrue(arm.sysIdDynamic(Direction.kReverse));
   }
 
   public void periodic() {
