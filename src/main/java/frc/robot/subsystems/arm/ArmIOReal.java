15f33f3b6fe97a3d391dc7072362d545206d8b03
Caden <Cachen15@s.sfusd.edu>
2025-02-12 17:02:37 -0800

    hi

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -45,13 +45,13 @@ public class ArmIOReal implements ArmIO {
     CurrentLimitsConfigs currentConfig = talonFXConfigs.CurrentLimits;
     FeedbackConfigs feedbackConfigs = talonFXConfigs.Feedback;
 
-    slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
-    slot0Configs.kG = 0; // Add 0 V to overcome gravity
-    slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-    slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-    slot0Configs.kP = 20; // A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kS = 0.18205; // Add 0.25 V output to overcome static friction
+    slot0Configs.kG = 0.09885; // Add 0 V to overcome gravity
+    slot0Configs.kV = 7.2427; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kA = 0.086264; // An acceleration of 1 rps/s requires 0.01 V output
+    slot0Configs.kP = 57.759; // A position error of 2.5 rotations results in 12 V output
     slot0Configs.kI = 0; // no output for integrated error
-    slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+    slot0Configs.kD = 8.4867; // A velocity error of 1 rps results in 0.1 V output
     slot0Configs.withGravityType(GravityTypeValue.Arm_Cosine);
 
     // feedbackConfigs.FeedbackRemoteSensorID = 0;
@@ -117,7 +117,7 @@ public class ArmIOReal implements ArmIO {
                 armPosition)
             .isOK());
 
-    DogLog.log("Arm/Motor/pid goal", armPIDGoal.getValueAsDouble());
+    DogLog.log("Arm/Motor/pid goal", Units.rotationsToDegrees(armPIDGoal.getValueAsDouble()));
     DogLog.log("Arm/Motor/motor voltage", armMotorVoltage.getValueAsDouble());
     DogLog.log("Arm/Motor/supply voltage", armSupplyVoltage.getValueAsDouble());
     DogLog.log("Arm/Motor/device temp", armDeviceTemp.getValueAsDouble());
