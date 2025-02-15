823aa86601bda3fb74dacdb7c071195e519f1626
kiwikimiko <chvoong@s.sfusd.edu>
2025-02-15 12:10:02 -0800

    arm changes

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -4,7 +4,7 @@ public class ArmConstants {
   public static final int ARM_MOTOR_ID = 15;
   public static final double MAX_VELOCITY = 1.2; // rotation per second
   public static final double MAX_ACCELERATION = 6; // rotation per second per second
-  public static final double ARM_GEAR_RATIO = 64.0;
+  public static final double ARM_GEAR_RATIO = 64.0; //12/68 * 20/84 * 16/48
 
   // Position consntants: in degrees
   public static final double L1_PREP_POSITION = 330;
