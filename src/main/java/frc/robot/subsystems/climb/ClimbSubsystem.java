050f26e5783f909b087cee5ad561d4cee82f82a8
Morgan Jiang <morganjiang9768@Gmail.com>
2025-02-15 12:02:29 -0800

    commands for sim

--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
@@ -12,6 +16,9 @@ public class ClimbSubsystem extends SubsystemBase {
     } else {
       climbIO = new ClimbIOReal();
     }
+
+    SmartDashboard.putData("Climb Command/turn to 60 degrees", setPosition(60));
+    SmartDashboard.putData("Climb Command/turn to 120 degrees", setPosition(120));
   }
 
   @Override
@@ -23,7 +30,11 @@ public class ClimbSubsystem extends SubsystemBase {
     return climbIO.getPosition();
   }
 
-  public void setPosition(double desiredPos) {
-    climbIO.setPosition(desiredPos);
+  public Command setPosition(double desiredPos) {
+    return this.runOnce(
+            () -> {
+              climbIO.setPosition(desiredPos);
+            })
+        .andThen(Commands.waitUntil(() -> MathUtil.isNear(desiredPos, climbIO.getPosition(), 0.1)));
   }
 }
