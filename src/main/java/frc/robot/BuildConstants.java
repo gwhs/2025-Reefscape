38c1af060834340cb10d602a5a727f2fb7f82312
Emma Kadlecek <kadlecekemma@gmail.com>
2025-02-17 10:16:22 -0800

    .

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-reefscape";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 144;
+  public static final String GIT_SHA = "de29e8a55850302655bca3c9bb771a466723095a";
+  public static final String GIT_DATE = "16-02-25 01:13:46";
+  public static final String GIT_BRANCH = "gradle-stuff";
+  public static final String BUILD_DATE = "17-02-25 18:15:28";
+  public static final long BUILD_UNIX_TIME = 1739816128066L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
