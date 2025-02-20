a5d80df46799b05262b10031406fc8de69bd9212
Maggie <maferrer2@s.sfusd.edu>
2025-02-19 19:16:40 -0800

    spotless

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-Reefscape";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 182;
+  public static final String GIT_SHA = "dbf4e6db284977051ca473920fc70c47642ef66b";
+  public static final String GIT_DATE = "20-02-25 03:14:22";
+  public static final String GIT_BRANCH = "objectDetection";
+  public static final String BUILD_DATE = "20-02-25 03:15:57";
+  public static final long BUILD_UNIX_TIME = 1740021357895L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
