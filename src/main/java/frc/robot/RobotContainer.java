1ebc78efc0124f1932283ce7cafaf854a1b8af8e
Emily Yu <EmilyYYu@hotmail.com>
2025-02-19 17:57:38 -0800

    ran spotless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -109,7 +109,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    
+
     configureBindings();
 
     configureAutonomous();
