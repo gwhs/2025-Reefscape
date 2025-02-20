e25016d60e9770d4a76f28ead1072e3b5b6be3c4
Maggie <maferrer2@s.sfusd.edu>
2025-02-19 18:52:24 -0800

    started on logic

--- src/main/java/frc/robot/subsystems/objectDetection/objectDetectionCam.java
+++ src/main/java/frc/robot/subsystems/objectDetection/objectDetectionCam.java
@@ -3,23 +3,31 @@ package frc.robot.subsystems.objectDetection;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.xml.crypto.dsig.Transform;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
 public class ObjectDetectionCam {
 
-  private final PhotonCamera cam;
+   private final PhotonCamera cam;
 
-  private final Alert visionNotConnected;
-
+    private final Supplier<Pose2d> robotPose;
     private int counter; 
-    private String ntKey;
+    private final String ntKey;
+    private final Transform3d robotToCam;
 
-    public ObjectDetectionCam(String name){
+    public ObjectDetectionCam(String name,Transform3d robotToCam,Supplier<Pose2d> robotPose){
         
         cam = new PhotonCamera(name); 
         counter = 0; 
-
+        this.robotPose = robotPose;
+        this.robotToCam = robotToCam;
        ntKey = "/Object Detection/" + name + "/";
     }
 
@@ -43,12 +51,26 @@ public class ObjectDetectionCam {
 
       for(PhotonPipelineResult result: results){
         PhotonTrackedTarget bestTarget = result.getBestTarget(); 
-        
+        Transform3d targetLocationToCamera = bestTarget.getBestCameraToTarget();
+        Transform3d targetLocationToField = this.getTargetLocInFieldSpace(targetLocationToCamera);
       }
 
     }
 
-    
+    //Transform (target location in camera space) by (camera location in field space) to get (target location in field space)
+    public Transform3d getTargetLocInFieldSpace(Transform3d targetLocationToCamera){
+
+      Pose2d robotPose = this.robotPose.get();
+      Pose3d robotPose3d = new Pose3d(robotPose);
+      Pose3d cameraPose3d = robotPose3d.plus(robotToCam);
+      DogLog.log(ntKey + "Camera Pose/", cameraPose3d);
+
+      Pose3d cameraToField = cameraPose3d.plus(targetLocationToCamera);
+      
+
+      return null ;
+    }
+
 
 
 }
