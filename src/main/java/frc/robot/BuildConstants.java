a2c529443d745de89e47eb71394087e130089402
Maggie <maferrer2@s.sfusd.edu>
2025-02-19 19:13:11 -0800

    spotless

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 151;
-  public static final String GIT_SHA = "e3b73465fab40d193f0f22888d225c954091f307";
-  public static final String GIT_DATE = "17-02-25 20:28:31";
-  public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "17-02-25 20:31:23";
-  public static final long BUILD_UNIX_TIME = 1739824283671L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 163;
+  public static final String GIT_SHA = "64b9599f8bfe99256058898e75ac64bac4671071";
+  public static final String GIT_DATE = "20-02-25 03:06:31";
+  public static final String GIT_BRANCH = "objectDetection";
+  public static final String BUILD_DATE = "20-02-25 03:12:25";
+  public static final long BUILD_UNIX_TIME = 1740021145260L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
