e13353530cf3ef31cf6addb37e790fb304aa5981
Maggie <maferrer2@s.sfusd.edu>
2025-02-19 17:54:27 -0800

    added getting all results

--- src/main/java/frc/robot/subsystems/objectDetection/objectDetectionCam.java
+++ src/main/java/frc/robot/subsystems/objectDetection/objectDetectionCam.java
@@ -1,17 +1,51 @@
 package frc.robot.subsystems.objectDetection;
 
-import org.photonvision.PhotonCamera;
+import java.util.List;
+import java.util.function.Supplier;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 public class ObjectDetectionCam {
 
-    private final PhotonCamera cam;
+  private final PhotonCamera cam;
+
+  private final Alert visionNotConnected;
+
+    private int counter; 
+    private String ntKey;
 
     public ObjectDetectionCam(String name){
+        
         cam = new PhotonCamera(name); 
+        counter = 0; 
+
+       ntKey = "/Object Detection/" + name + "/";
     }
 
+
+    //1. Get all results from the camera
+    //2. Check if the list is empty
+    //3. If not empty, loop through and getBestTarget
+    //4. Get target location in camera space
+    //5. Transform (target location in camera space) by (camera location in field space) to get (target location in field space)
+    //6. Use three PID controllers x, y, and rotation to drive and rotate robot to target pose
     public void updateDetection(){
         
+      counter ++ ; 
+      List<PhotonPipelineResult> results = cam.getAllUnreadResults();
+      DogLog.log(ntKey + "Number of Results/", results.size());
+      DogLog.log(ntKey + "counter", counter);
+      
+      if(results.isEmpty()){
+        return; 
+      }
+
+      for(PhotonPipelineResult result: results){
+        PhotonTrackedTarget bestTarget = result.getBestTarget(); 
+        
+      }
+
     }
 
     
