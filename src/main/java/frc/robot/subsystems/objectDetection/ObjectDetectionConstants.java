fd74bb66b0fc550680534bcdd7a1a1c6e6819c49
Maggie <maferrer2@s.sfusd.edu>
2025-02-19 19:06:15 -0800

    added example robot to cam

--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionConstants.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionConstants.java
@@ -1,6 +1,20 @@
 package frc.robot.subsystems.objectDetection;
 
+import javax.xml.crypto.dsig.Transform;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+
 public class ObjectDetectionConstants {
 
   private final double minConfindence = 0.75;
+  public static final Transform3d robotToCam =
+      new Transform3d(
+          Units.inchesToMeters(9.6),
+          Units.inchesToMeters(11.2),
+          Units.inchesToMeters(8),
+          new Rotation3d(
+              Units.degreesToRadians(0), Units.degreesToRadians(-20), Units.degreesToRadians(-5)));
+
 }
