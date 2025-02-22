de583f0a30abbd3949e16a718e6ebe751a27e177
Caden <Cachen15@s.sfusd.edu>
2025-02-22 10:49:35 -0800

    helper method for auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 199;
-  public static final String GIT_SHA = "20bf6a12ad0796a932320e7878c37200b9ad26f7";
-  public static final String GIT_DATE = "20-02-25 02:26:02";
+  public static final int GIT_REVISION = 202;
+  public static final String GIT_SHA = "2c9f7ffefb5fd5c2623e62a8574f38bfaad3eb3c";
+  public static final String GIT_DATE = "20-02-25 03:25:36";
   public static final String GIT_BRANCH = "autonomous-improvements";
-  public static final String BUILD_DATE = "20-02-25 02:49:25";
-  public static final long BUILD_UNIX_TIME = 1740019765457L;
+  public static final String BUILD_DATE = "22-02-25 18:47:43";
+  public static final long BUILD_UNIX_TIME = 1740250063362L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/autonomous/FiveCycleProcessor.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycleProcessor.java
@@ -10,6 +10,7 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.EagleUtil;
 import frc.robot.RobotContainer;
@@ -17,8 +18,14 @@ import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.elevator.ElevatorConstants;
 
 public class FiveCycleProcessor extends PathPlannerAuto {
+
+  private RobotContainer robotContainer;
+  private double waitTime = 0.3;
+
   public FiveCycleProcessor(RobotContainer robotContainer) {
     super(Commands.run(() -> {}));
+
+    this.robotContainer = robotContainer;
 
     try {
       PathPlannerPath SC_F = PathPlannerPath.fromPathFile("SC-F");
@@ -32,121 +39,53 @@ public class FiveCycleProcessor extends PathPlannerAuto {
       PathPlannerPath CSP_B = PathPlannerPath.fromPathFile("CSP-B");
       PathPlannerPath B_CSP = PathPlannerPath.fromPathFile("B-CSP");
 
-      double waitTime = 0.3;
-
       Pose2d startingPose =
           new Pose2d(SC_F.getPoint(0).position, SC_F.getIdealStartingState().rotation());
 
       isRunning()
           .onTrue(
               Commands.sequence(
-                      AutoBuilder.resetOdom(startingPose).onlyIf(() -> RobotBase.isSimulation()),
-                      AutoBuilder.followPath(SC_F)
-                          .deadlineFor(
-                              Commands.sequence(
-                                  robotContainer.zeroElevator(),
-                                  robotContainer.prepScoreCoral(
-                                      ElevatorConstants.L4_PREP_POSITION,
-                                      ArmConstants.L4_PREP_POSITION))))
-                  .withName("Leave SC"));
-
-      event("atF")
-          .onTrue(
-              Commands.sequence(
-                      robotContainer
-                          .prepScoreCoral(
-                              ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
-                          .deadlineFor(
-                              robotContainer.alignToPose(
-                                  () ->
-                                      EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
-                      robotContainer.scoreCoral(),
-                      AutoBuilder.followPath(F_CSP).alongWith(robotContainer.prepCoralIntake()))
-                  .withName("Score preload at F"));
-
-      event("atCSP_F")
-          .onTrue(
-              Commands.sequence(
-                      Commands.waitSeconds(waitTime),
-                      AutoBuilder.followPath(CSP_E)
-                          .deadlineFor(
-                              Commands.sequence(
-                                  Commands.waitSeconds(0.1),
-                                  robotContainer.prepScoreCoral(
-                                      ElevatorConstants.L4_PREP_POSITION,
-                                      ArmConstants.L4_PREP_POSITION))),
-                      robotContainer
-                          .prepScoreCoral(
-                              ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
-                          .deadlineFor(
-                              robotContainer.alignToPose(
-                                  () ->
-                                      EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
-                      robotContainer.scoreCoral(),
-                      AutoBuilder.followPath(E_CSP).alongWith(robotContainer.prepCoralIntake()))
-                  .withName("CSP to E"));
-
-      event("atCSP_E")
-          .onTrue(
-              Commands.sequence(
-                      Commands.waitSeconds(waitTime),
-                      AutoBuilder.followPath(CSP_D)
-                          .deadlineFor(
-                              Commands.sequence(
-                                  Commands.waitSeconds(0.1),
-                                  robotContainer.prepScoreCoral(
-                                      ElevatorConstants.L4_PREP_POSITION,
-                                      ArmConstants.L4_PREP_POSITION))),
-                      robotContainer
-                          .prepScoreCoral(
-                              ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
-                          .deadlineFor(
-                              robotContainer.alignToPose(
-                                  () ->
-                                      EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
-                      robotContainer.scoreCoral(),
-                      AutoBuilder.followPath(D_CSP).alongWith(robotContainer.prepCoralIntake()))
-                  .withName("CSP to D"));
-
-      event("atCSP_D")
-          .onTrue(
-              Commands.sequence(
-                      Commands.waitSeconds(waitTime),
-                      AutoBuilder.followPath(CSP_C)
-                          .deadlineFor(
-                              Commands.sequence(
-                                  Commands.waitSeconds(0.1),
-                                  robotContainer.prepScoreCoral(
-                                      ElevatorConstants.L4_PREP_POSITION,
-                                      ArmConstants.L4_PREP_POSITION))),
-                      robotContainer
-                          .prepScoreCoral(
-                              ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
-                          .deadlineFor(
-                              robotContainer.alignToPose(
-                                  () ->
-                                      EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
-                      robotContainer.scoreCoral(),
-                      AutoBuilder.followPath(C_CSP).alongWith(robotContainer.prepCoralIntake()))
-                  .withName("CSP to C"));
-
-      /*event("atCSP_C")
-      .onTrue(
-          Commands.sequence(
-                  Commands.waitSeconds(waitTime),
-                  AutoBuilder.followPath(CSP_B).alongWith(robotContainer.coralHandoff()),
+                  AutoBuilder.resetOdom(startingPose).onlyIf(() -> RobotBase.isSimulation()),
+                  AutoBuilder.followPath(SC_F)
+                      .deadlineFor(
+                          Commands.sequence(
+                              robotContainer.zeroElevator().onlyIf(() -> RobotBase.isReal()),
+                              robotContainer.prepScoreCoral(
+                                  ElevatorConstants.L4_PREP_POSITION,
+                                  ArmConstants.L4_PREP_POSITION))),
                   robotContainer
-                      .prepScoreCoralL4()
+                      .prepScoreCoral(
+                          ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
                       .deadlineFor(
                           robotContainer.alignToPose(
-                              () ->
-                                  EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
+                              () -> EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
                   robotContainer.scoreCoral(),
-                  AutoBuilder.followPath(B_CSP).alongWith(robotContainer.prepCoralIntake()))
-              .withName("CSP to B"));*/
+                  AutoBuilder.followPath(F_CSP).alongWith(robotContainer.prepCoralIntake()),
+                  autoHelper(CSP_E, E_CSP),
+                  autoHelper(CSP_D, D_CSP),
+                  autoHelper(CSP_C, C_CSP)));
+
 
     } catch (Exception e) {
       DriverStation.reportError("Path Not Found: " + e.getMessage(), e.getStackTrace());
     }
+  }
+
+  public Command autoHelper(PathPlannerPath pathOne, PathPlannerPath pathTwo) {
+    return Commands.sequence(
+        Commands.waitSeconds(waitTime),
+        AutoBuilder.followPath(pathOne)
+            .deadlineFor(
+                Commands.sequence(
+                    Commands.waitSeconds(0.1),
+                    robotContainer.prepScoreCoral(
+                        ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION))),
+        robotContainer
+            .prepScoreCoral(ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION)
+            .deadlineFor(
+                robotContainer.alignToPose(
+                    () -> EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
+        robotContainer.scoreCoral(),
+        AutoBuilder.followPath(pathTwo).alongWith(robotContainer.prepCoralIntake()));
   }
 }
