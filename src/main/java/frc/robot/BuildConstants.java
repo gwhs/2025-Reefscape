148256a01b712a6434287612515d48f0198d7554
Caden <Cachen15@s.sfusd.edu>
2025-02-22 16:17:14 -0800

    tweaks

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 188;
-  public static final String GIT_SHA = "021bee1d6a37d281aa6552383177bcf19359fc46";
-  public static final String GIT_DATE = "22-02-25 23:51:00";
+  public static final int GIT_REVISION = 189;
+  public static final String GIT_SHA = "ed13a4beeb3c6aa1eb0a6e697aa9c34167a377d1";
+  public static final String GIT_DATE = "23-02-25 00:15:46";
   public static final String GIT_BRANCH = "compTesting";
-  public static final String BUILD_DATE = "23-02-25 00:10:59";
-  public static final long BUILD_UNIX_TIME = 1740269459000L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "23-02-25 00:15:46";
+  public static final long BUILD_UNIX_TIME = 1740269746827L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
