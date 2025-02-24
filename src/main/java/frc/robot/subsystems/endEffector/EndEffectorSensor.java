8006ab84e51f1767ae4c0c5a5fe61ae52876b2cd
No767 <73260931+No767@users.noreply.github.com>
2025-02-24 01:03:23 -0800

    Add sensor code

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorSensor.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorSensor.java
@@ -1,29 +1,16 @@
-
 package frc.robot.subsystems.endEffector;
 
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
 public class EndEffectorSensor {
-    TimeOfFlight (int sensorID) {
-        
+    private TimeOfFlight sensor = new TimeOfFlight(EndEffectorConstants.TOF_DEVICE_ID);
+    
+    public EndEffectorSensor() {
+        sensor.setRangingMode(RangingMode.Short, 0.24); // Will need to test. must be between 24-1000ms
     }
 
-    public static enum TimeOfFlight.RangingMode; 
-    
-
-    public static final TimeOfFlight.RangingMode Short;
-
-    public void robotPeriodic {
-        public double getRange() {
-
-        }
-
-        public TimeOfFlight.RangingMode getRangingMode() {
-
-        }
-
-        public void setRangingMode(TimeOfFlight.RangingMode mode, double sampleTime) {
-
-        }
-
+    public double getRange() {
+        return sensor.getRange();
     }
 
 }
