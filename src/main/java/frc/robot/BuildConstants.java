282849c23afde8c072dda63de42835dee37baba3
Caton Q <caton868@gmail.com>
2025-02-24 16:14:31 -0800

    we added some changes to the robot container

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 183;
-  public static final String GIT_SHA = "7cfbe7f8e2aa57c11ef0a6e6116c148b887d0817";
-  public static final String GIT_DATE = "23-02-25 03:16:44";
+  public static final int GIT_REVISION = 184;
+  public static final String GIT_SHA = "b1fac13617e411117865fa7383182422ec782c85";
+  public static final String GIT_DATE = "25-02-25 00:02:37";
   public static final String GIT_BRANCH = "improvements";
-  public static final String BUILD_DATE = "24-02-25 23:59:28";
-  public static final long BUILD_UNIX_TIME = 1740441568371L;
+  public static final String BUILD_DATE = "25-02-25 00:05:48";
+  public static final long BUILD_UNIX_TIME = 1740441948080L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
