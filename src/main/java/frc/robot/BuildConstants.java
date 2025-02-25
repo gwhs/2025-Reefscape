b3ce0074622de992499931682e520574b959dd88
Caton Q <caton868@gmail.com>
2025-02-24 16:18:18 -0800

    added some stuff to arm subsystem

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 184;
-  public static final String GIT_SHA = "b1fac13617e411117865fa7383182422ec782c85";
-  public static final String GIT_DATE = "25-02-25 00:02:37";
+  public static final int GIT_REVISION = 186;
+  public static final String GIT_SHA = "f867e2726022a80b26c56cf7378628b177525b88";
+  public static final String GIT_DATE = "25-02-25 00:17:19";
   public static final String GIT_BRANCH = "improvements";
-  public static final String BUILD_DATE = "25-02-25 00:05:48";
-  public static final long BUILD_UNIX_TIME = 1740441948080L;
+  public static final String BUILD_DATE = "25-02-25 00:17:19";
+  public static final long BUILD_UNIX_TIME = 1740442639619L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
