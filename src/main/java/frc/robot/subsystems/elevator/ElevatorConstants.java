831175560f6862ac62187720ce17d82ecfa3c419
Caden <Cachen15@s.sfusd.edu>
2025-02-26 17:26:27 -0800

    spotless

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -33,10 +33,6 @@ public class ElevatorConstants {
   public static final double DEALGAE_LOW_POSITION = 0;
   public static final double DEALGAE_HIGH_POSITION = 30;
   public static final double DEALGAE_STOW_POSITION = 0;
-  // placeh
-
-  public static final double INTAKE_METER =
-      ElevatorSubsystem.rotationsToMeters(0 / GEAR_RATIO_DIFF);
 
   public static final double STOW_METER = ElevatorSubsystem.rotationsToMeters(15 / GEAR_RATIO_DIFF);
 
