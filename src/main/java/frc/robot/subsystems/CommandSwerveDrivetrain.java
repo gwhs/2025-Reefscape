50c917bba2b1486012b63568f50e16c1f313dad5
Caden <Cachen15@s.sfusd.edu>
2025-02-26 18:53:23 -0800

    Update at 'Wed Feb 26 18:53:23 PST 2025'

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -74,7 +74,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   CurrentLimitsConfigs m3_current_config = new CurrentLimitsConfigs();
   CurrentLimitsConfigs m4_current_config = new CurrentLimitsConfigs();
 
-  public Constraints constraints = new TrapezoidProfile.Constraints(3, 3);
+  public Constraints constraints = new TrapezoidProfile.Constraints(3, 1);
   public ProfiledPIDController PID_X = new ProfiledPIDController(3, 0, 0, constraints);
   public ProfiledPIDController PID_Y = new ProfiledPIDController(3, 0, 0, constraints);
 
