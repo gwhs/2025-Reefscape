ec0237b69bee089169af9fa3050d76e4f7ed1fac
Emma Kadlecek <kadlecekemma@gmail.com>
2025-03-01 10:21:33 -0800

    .

--- src/main/java/frc/robot/subsystems/groundIntake/groundIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/groundIntake/groundIntakeSubsystem.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems.groundIntake;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class groundIntakeSubsystem {
+public class groundIntakeSubsystem extends SubsystemBase {
 
   groundIntakeIO groundintakeIO;
 
@@ -10,7 +13,28 @@ public class groundIntakeSubsystem {
     if (RobotBase.isSimulation()) {
       groundintakeIO = new groundIntakeIOSim();
     } else {
-      groundintakeIO = new groundIntakeIOReal();
+      groundintakeIO = new groun
+	      dIntakeIOReal();
     }
   }
+	/**
+	 * @param voltage the voltage to set it to
+	 * @return run the command
+	 */
+	public Command setPivotMot`orVoltage(double voltage) {
+		return Commands.runOnce( () -> groundintakeIO.setPivotMotorVoltage(voltage));
+	}
+
+	/**
+	 * @param voltage the voltage to set it to
+	 * @return run the command
+	 */ 
+	public Command setSpinMotorVoltage(double voltage) {
+		return Commands.runOnce( () -> groundintakeIO.setSpinMotorVoltage(voltage));
+	}
+
+	@Override
+	public void periodic() {
+		groundintakeIO.update();
+	}
 }
