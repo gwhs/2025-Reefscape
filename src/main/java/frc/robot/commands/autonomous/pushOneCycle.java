17878bfe5273a0778f534138fc2bc63cf75eadd5
bella <betan1@s.sfusd.edu>
2025-03-01 11:38:51 -0800

    push one cycle

---
+++ src/main/java/frc/robot/commands/autonomous/pushOneCycle.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.autonomous;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.arm.ArmConstants;
+import frc.robot.subsystems.elevator.ElevatorConstants;
+
+public class pushOneCycle extends PathPlannerAuto {
+  public pushOneCycle(RobotContainer robotContainer) {
+    super(Commands.run(() -> {}));
+
+    /* All your code should go inside this try-catch block */
+    try {
+
+      PathPlannerPath startLn_H = PathPlannerPath.fromPathFile("(PC1) Startline-H");
+
+      Pose2d startingPose =
+          new Pose2d(startLn_H.getPoint(0).position, startLn_H.getIdealStartingState().rotation());
+
+      isRunning()
+          .onTrue(
+              Commands.sequence(
+                      AutoBuilder.resetOdom(startingPose),
+                      AutoBuilder.followPath(startLn_H),
+                      robotContainer.prepScoreCoral(
+                          ElevatorConstants.L1_PREP_POSITION, ArmConstants.L1_PREP_POSITION),
+                      robotContainer.scoreCoral())
+                  .withName("Leave Startline (Push) and score L1 at H"));
+
+    } catch (Exception e) {
+      DriverStation.reportError("Path Not Found: " + e.getMessage(), e.getStackTrace());
+    }
+  }
+}
