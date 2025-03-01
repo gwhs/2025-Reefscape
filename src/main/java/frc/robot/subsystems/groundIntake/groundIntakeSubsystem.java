2badd9f5ffda6f37ac25ee8e380515cab9fa431c
Emma Kadlecek <kadlecekemma@gmail.com>
2025-03-01 10:23:55 -0800

    .

--- src/main/java/frc/robot/subsystems/groundIntake/groundIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/groundIntake/groundIntakeSubsystem.java
@@ -13,28 +13,28 @@ public class groundIntakeSubsystem extends SubsystemBase {
     if (RobotBase.isSimulation()) {
       groundintakeIO = new groundIntakeIOSim();
     } else {
-      groundintakeIO = new groun
-	      dIntakeIOReal();
+      groundintakeIO = new groundIntakeIOReal();
     }
   }
-	/**
-	 * @param voltage the voltage to set it to
-	 * @return run the command
-	 */
-	public Command setPivotMot`orVoltage(double voltage) {
-		return Commands.runOnce( () -> groundintakeIO.setPivotMotorVoltage(voltage));
-	}
 
-	/**
-	 * @param voltage the voltage to set it to
-	 * @return run the command
-	 */ 
-	public Command setSpinMotorVoltage(double voltage) {
-		return Commands.runOnce( () -> groundintakeIO.setSpinMotorVoltage(voltage));
-	}
+  /**
+   * @param voltage the voltage to set it to
+   * @return run the command
+   */
+  public Command setPivotMotorVoltage(double voltage) {
+    return Commands.runOnce(() -> groundintakeIO.setPivotMotorVoltage(voltage));
+  }
 
-	@Override
-	public void periodic() {
-		groundintakeIO.update();
-	}
+  /**
+   * @param voltage the voltage to set it to
+   * @return run the command
+   */
+  public Command setSpinMotorVoltage(double voltage) {
+    return Commands.runOnce(() -> groundintakeIO.setSpinMotorVoltage(voltage));
+  }
+
+  @Override
+  public void periodic() {
+    groundintakeIO.update();
+  }
 }
