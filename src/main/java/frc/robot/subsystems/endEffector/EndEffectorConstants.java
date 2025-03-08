087e7fc7c32bcc5eedf3c0549c6c2057f0304922
Zhyang4916 <2006zhide@gmail.com>
2025-03-08 14:46:08 -0800

    removes some REV colorsensor code and imports, changes to variable names, integrates TOF code to files

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstants.java
@@ -4,7 +4,7 @@ public class EndEffectorConstants {
 
   public static final int deviceID = 16;
   public static final int GEAR_RATIO = 3;
-  public static final int TOF_DEVICE_ID = 2; // Assume 0 for now
+  public static final int CORAL_DETECTOR_ID = 2; // Assume 0 for now
   public static final double EXP_DECAY = 0.02; // exponential decay rate for EMA
   public static final double SDEV_Decay = 0.02;
   public static final double HOLD_CORAL_CURRENT = 10.0;
