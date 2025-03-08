7e299bab41ebab5ee9f1f5b870aa74b0d1492a2f
Caton Q <caton868@gmail.com>
2025-03-08 11:45:57 -0800

    i ran spotleses

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -284,9 +284,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 m_hasAppliedOperatorPerspective = true;
               });
 
-      DogLog.log("Command Swerve DriveTrain/is Aligning to pose", IS_ALIGNING_TO_POSE.getAsBoolean());
+      DogLog.log(
+          "Command Swerve DriveTrain/is Aligning to pose", IS_ALIGNING_TO_POSE.getAsBoolean());
       DogLog.log("Command Swerve DriveTrain/is at target pose", IS_AT_TARGET_POSE.getAsBoolean());
-
     }
 
     DogLog.log("Swerve/current X setpoint", PID_X.getSetpoint().position);
