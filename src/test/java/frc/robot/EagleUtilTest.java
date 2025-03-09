a894d181a647a73efecba7a616ae2513e0f717af
Rob Blair <robtheblair@gmail.com>
2025-03-08 16:08:48 -0800

    verify findClosestReefIndex

--- src/test/java/frc/robot/EagleUtilTest.java
+++ src/test/java/frc/robot/EagleUtilTest.java
@@ -2,21 +2,42 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class EagleUtilTest {
 
-  @Test
-  public void testClosestTo0() {
+  @BeforeAll
+  public static void setupEagleUtils() {
     EagleUtil.calculateRedReefSetPoints();
     EagleUtil.calculateBlueReefSetPoints();
-    EagleUtil.setAllianceOverride(false);
+  }
 
+  @Test
+  public void testClosestTo0() {
+
+    EagleUtil.setAllianceOverride(false);
     Pose2d closest = EagleUtil.closestReefSetPoint(EagleUtil.m_bluePoses.get(0), 0);
     Assertions.assertEquals(EagleUtil.m_bluePoses.get(0), closest);
 
     EagleUtil.setAllianceOverride(true);
     closest = EagleUtil.closestReefSetPoint(EagleUtil.m_redPoses.get(0), 0);
     Assertions.assertEquals(EagleUtil.m_redPoses.get(0), closest);
+  }
+
+  @Test
+  public void testIndexTest() {
+
+    EagleUtil.setAllianceOverride(false);
+    for (int i = 0; i < EagleUtil.m_bluePoses.size(); i++) {
+      int index = EagleUtil.findClosestReefIndex(EagleUtil.m_bluePoses.get(i));
+      Assertions.assertEquals(i, index);
+    }
+
+    EagleUtil.setAllianceOverride(true);
+    for (int i = 0; i < EagleUtil.m_redPoses.size(); i++) {
+      int index = EagleUtil.findClosestReefIndex(EagleUtil.m_redPoses.get(i));
+      Assertions.assertEquals(i, index);
+    }
   }
 }
