aaec1e25b378925b6bad6c6b6234542e95a6771d
Caden Chen <Cachen15@s.sfusd.edu>
2025-03-09 14:47:59 -0700

    Update at 'Sun Mar 09 14:47:59 PDT 2025'

--- src/main/java/frc/robot/commands/autonomous/FiveCycle.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycle.java
@@ -20,7 +20,7 @@ import frc.robot.subsystems.elevator.ElevatorConstants;
 public class FiveCycle extends PathPlannerAuto {
 
   private RobotContainer robotContainer;
-  private double waitTime = 2;
+  private double waitTime = 1.5;
 
   public FiveCycle(RobotContainer robotContainer, boolean nonProcessorSide) {
     super(Commands.run(() -> {}));
