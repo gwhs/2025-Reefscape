79230d35f535951c615fa9e777fcd68cf00a9166
Caton Q <caton868@gmail.com>
2025-03-12 18:01:45 -0700

    spotless

--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -9,5 +9,5 @@ public class ClimbConstants {
 
   public static final double CLIMB_CLIMB_POSITION = 2.77;
   public static final double STOW_CLIMB_POSITION = 0;
-  public static final double LATCH_CLIMB_POSITION = 0.8; 
+  public static final double LATCH_CLIMB_POSITION = 0.8;
 }
