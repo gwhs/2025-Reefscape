90fc88fecf036aff61e9f88cdd97ed4aeeff45d1
Maggie <maferrer2@s.sfusd.edu>
2025-03-17 19:03:09 -0700

    yay

--- src/main/java/frc/robot/commands/autonomous/FiveCycle.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycle.java
@@ -96,7 +96,7 @@ public class FiveCycle extends PathPlannerAuto {
                     Commands.waitSeconds(.8),
                     robotContainer.prepScoreCoral(
                         ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION))),
-        Commands.sequence(Commands.waitSeconds(.2), robotContainer.autonScoreCoral())
+        Commands.sequence(Commands.waitSeconds(.1, robotContainer.autonScoreCoral())
             .deadlineFor(
                 robotContainer.alignToPose(
                     () -> EagleUtil.getCachedReefPose(robotContainer.getRobotPose()))),
