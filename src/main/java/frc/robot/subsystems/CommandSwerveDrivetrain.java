8464651d36b540d522248af8c4d3a1e4fc5daac2
Caden <Cachen15@s.sfusd.edu>
2025-03-18 18:27:40 -0700

    Update at 'Tue Mar 18 18:27:40 PDT 2025'

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -52,10 +52,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
           });
 
   public Constraints constraints = new TrapezoidProfile.Constraints(3, 2);
-  public ProfiledPIDController PID_X = new ProfiledPIDController(5.0, 0, 0, constraints);
-  public ProfiledPIDController PID_Y = new ProfiledPIDController(5.0, 0, 0, constraints);
+  public ProfiledPIDController PID_X = new ProfiledPIDController(4.5, 0, 0, constraints);
+  public ProfiledPIDController PID_Y = new ProfiledPIDController(4.5, 0, 0, constraints);
 
-  public PIDController PID_Rotation = new PIDController(0.5, 0, 0);
+  public PIDController PID_Rotation = new PIDController(0.2, 0, 0);
   public Trigger IS_AT_TARGET_POSE =
       new Trigger(() -> PID_X.atSetpoint() && PID_Y.atSetpoint() && PID_Rotation.atSetpoint());
 
