136b95133cbfb8c4de1ff2c23bae3a0a4058c48e
Caden <Cachen15@s.sfusd.edu>
2025-03-19 19:33:53 -0700

    Update at 'Wed Mar 19 19:33:53 PDT 2025'

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorSubsystem.java
@@ -47,7 +47,7 @@ public class EndEffectorSubsystem extends SubsystemBase {
 
   public Command intake() {
     return Commands.runOnce(
-        () -> endEffectorIO.setAmps(EndEffectorConstants.INTAKE_CORAL_CURRENT, 0.25));
+        () -> endEffectorIO.setAmps(EndEffectorConstants.INTAKE_CORAL_CURRENT, 0.35));
   }
 
   public Command holdCoral() {
