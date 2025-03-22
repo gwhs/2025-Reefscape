8966ac5619052daa2feca050f31a57aa1a6f9143
Caton Q <caton868@gmail.com>
2025-03-22 12:34:33 -0700

    Update at 'Sat Mar 22 12:34:33 PDT 2025'

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -14,7 +14,7 @@ public class ArmConstants {
   public static final double L3_PREP_POSITION = 124.565;
   public static final double L4_PREP_POSITION = 120;
 
-  public static final double ARM_UPPER_BOUND = 180;
+  public static final double ARM_UPPER_BOUND = 270;
   public static final double ARM_LOWER_BOUND = -170;
 
   public static final double ARM_INTAKE_ANGLE = -73.477;
