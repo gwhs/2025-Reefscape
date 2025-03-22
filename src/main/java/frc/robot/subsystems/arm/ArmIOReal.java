0bd493683121104e4bb33f018e38f325a4fccf46
Caton Q <caton868@gmail.com>
2025-03-22 12:54:23 -0700

    Update at 'Sat Mar 22 12:54:23 PDT 2025'

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -70,7 +70,7 @@ public class ArmIOReal implements ArmIO {
     slot0Configs.kG = 0.5; // Add 0 V to overcome gravity
     slot0Configs.kV = 7.2427; // A velocity target of 1 rps results in 0.12 V output
     slot0Configs.kA = 0.086264; // An acceleration of 1 rps/s requires 0.01 V output
-    slot0Configs.kP = 20.759; // A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kP = 24.759; // A position error of 2.5 rotations results in 12 V output
     slot0Configs.kI = 0; // no output for integrated error
     slot0Configs.kD = 8.4867; // A velocity error of 1 rps results in 0.1 V output
     slot0Configs.withGravityType(GravityTypeValue.Arm_Cosine);
