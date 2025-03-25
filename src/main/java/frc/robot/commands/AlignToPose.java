f1c287525ec1bbfd259cb2032b99ed99bfd2fad8
Zhyang4916 <2006zhide@gmail.com>
2025-03-24 18:49:14 -0700

    changes PID for drive commands and auto align for rotational output, increases PID

--- src/main/java/frc/robot/commands/AlignToPose.java
+++ src/main/java/frc/robot/commands/AlignToPose.java
@@ -36,6 +36,7 @@ public class AlignToPose extends Command {
   private long startTime;
 
   public static final double PID_MAX = 0.44;
+  public static final double PID_ROTATION_MAX = 0.70;
 
   private final SwerveRequest.FieldCentric drive =
       new SwerveRequest.FieldCentric()
@@ -111,7 +112,7 @@ public class AlignToPose extends Command {
     DogLog.log("Align/PIDYoutput", PIDYOutput);
 
     double PIDRotationOutput =
-        MathUtil.clamp(drivetrain.PID_Rotation.calculate(currRotation), -PID_MAX, PID_MAX);
+        MathUtil.clamp(drivetrain.PID_Rotation.calculate(currRotation), -PID_ROTATION_MAX, PID_ROTATION_MAX);
     double angularVelocity = PIDRotationOutput;
     DogLog.log("Align/PIDRotationoutput", PIDRotationOutput);
 
