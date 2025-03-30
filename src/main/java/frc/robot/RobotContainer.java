3c151370eb476f218f8af4315976c3a90e7248c9
Caden <Cachen15@s.sfusd.edu>
2025-03-30 14:31:14 -0700

    Update at 'Sun Mar 30 14:31:14 PDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -753,9 +753,11 @@ public class RobotContainer {
     return Commands.sequence(
         arm.setAngle(ArmConstants.CLIMB_ANGLE).withTimeout(1),
         Commands.runOnce(() -> driveCommand.setTargetMode(DriveCommand.TargetMode.REEF)),
-        climb.stow(),
+        climb.stow().withTimeout(5),
         elevator.setHeight(ElevatorConstants.STOW_METER).withTimeout(1),
-        arm.setAngle(ArmConstants.ARM_STOW_ANGLE));
+        arm.setAngle(ArmConstants.ARM_STOW_ANGLE))
+        .withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
+        .withName("unPrepClimb");
   }
 
   public Command climb() {
