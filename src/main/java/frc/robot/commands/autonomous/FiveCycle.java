efc22cd4f5e812701d450280a3802dbeea6f7f75
Caden <Cachen15@s.sfusd.edu>
2025-03-30 11:39:28 -0700

    Update at 'Sun Mar 30 11:39:28 PDT 2025'

--- src/main/java/frc/robot/commands/autonomous/FiveCycle.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycle.java
@@ -99,7 +99,7 @@ public class FiveCycle extends PathPlannerAuto {
                         .prepScoreCoral(
                             ElevatorConstants.INTAKE_METER, ArmConstants.L4_PREP_POSITION)
                         .withTimeout(0.02),
-                    Commands.waitSeconds(0.6),
+                    Commands.waitSeconds(0.5),
                     robotContainer.prepScoreCoral(
                         ElevatorConstants.L4_PREP_POSITION, ArmConstants.L4_PREP_POSITION))),
         Commands.sequence(
