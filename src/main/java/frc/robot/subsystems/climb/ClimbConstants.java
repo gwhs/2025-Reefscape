3cd1aa3e7be4a30ce07ceb39bcda23b802bd8725
Caden <Cachen15@s.sfusd.edu>
2025-03-29 18:13:29 -0700

    changes

--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -5,7 +5,7 @@ public class ClimbConstants {
   public static final double CLIMB_GEAR_RATIO = 100.0;
 
   public static final double MAX_ACCELERATION = 1.8;
-  public static final double MAX_VELOCITY = 1.8;
+  public static final double MAX_VELOCITY = 1.4;
 
   public static final double CLIMB_CLIMB_POSITION = 2.7; // 2.77
   public static final double STOW_CLIMB_POSITION = 0;
