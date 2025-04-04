1ebadb6bac875a14a730c8089c5f43b82d32ae2e
Caden Chen <Cachen15@s.sfusd.edu>
2025-04-04 16:36:43 -0700

    lowered accel

--- src/main/java/frc/robot/commands/autonomous/FiveCycle.java
+++ src/main/java/frc/robot/commands/autonomous/FiveCycle.java
@@ -88,7 +88,7 @@ public class FiveCycle extends PathPlannerAuto {
   public Command autoHelper(PathPlannerPath pathOne, PathPlannerPath pathTwo) {
     return Commands.sequence(
         // wait until coral is loaded
-        // Commands.waitUntil(robotContainer.IS_CORAL_LOADED),
+        Commands.waitUntil(robotContainer.IS_CORAL_LOADED),
         // Commands.waitSeconds(waitTime),
         // drive to scoring position
         AutoBuilder.followPath(pathOne)
