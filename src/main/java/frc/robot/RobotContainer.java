822d377af3f1d8d88aa0c84461d2e145e1e9276b
Caden <Cachen15@s.sfusd.edu>
2025-04-29 16:14:47 -0700

    Update at 'Tue Apr 29 16:14:47 PDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -718,7 +718,7 @@ public class RobotContainer {
                                     ALGAE_HIGH))
                             .withTimeout(0.4)), // .5
                 Commands.either(prepDealgaeHigh(), prepDealgaeLow(), ALGAE_HIGH)
-                    .withTimeout(.5) // .6
+                    .withTimeout(.2) // .6
                     .deadlineFor(
                         alignToPose(() -> EagleUtil.getNearestAlgaePoint(drivetrain.getPose()))),
                 dealgae())
