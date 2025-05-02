e3c7bdf4872dd9c6212fb4814bf8c66011ac1eea
Caden740 <118308692+Caden740@users.noreply.github.com>
2025-05-01 21:52:05 -0700

    auto align to back l1/l2 (#294)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -458,13 +458,31 @@ public class RobotContainer {
                     })
                 .withName("Slow and Robot Centric"));
 
-    m_driverController
-        .a()
+    IS_L1
+        .and(m_driverController.a())
+        .whileTrue(alignToPose(() -> EagleUtil.getClosestL1Back(drivetrain.getPose(0.25))));
+
+    IS_L2
+        .and(m_driverController.a())
+        .whileTrue(alignToPose(() -> EagleUtil.getClosestLeftReefBack(drivetrain.getPose(0.25))));
+
+    IS_L4
+        .or(IS_L3)
+        .and(m_driverController.a())
         .whileTrue(alignToPose(() -> EagleUtil.getClosestLeftReef(drivetrain.getPose(0.25))));
 
-    m_driverController
-        .b()
+    IS_L4
+        .or(IS_L3)
+        .and(m_driverController.b())
         .whileTrue(alignToPose(() -> EagleUtil.getClosestRightReef(drivetrain.getPose(0.25))));
+
+    IS_L2
+        .and(m_driverController.b())
+        .whileTrue(alignToPose(() -> EagleUtil.getClosestRightReefBack(drivetrain.getPose(0.25))));
+
+    IS_L1
+        .and(m_driverController.b())
+        .whileTrue(alignToPose(() -> EagleUtil.getClosestL1Back(drivetrain.getPose(0.25))));
 
     m_operatorController.start().onTrue(elevator.homingCommand());
 
