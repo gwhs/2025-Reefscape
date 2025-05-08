3f05d7a1b0097bb3a0538616e1c6098e23f3a3b9
Caton Q <caton868@gmail.com>
2025-05-07 18:31:00 -0700

    Update at 'Wed May 07 18:31:00 PDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -434,6 +434,7 @@ public class RobotContainer {
     //             .withName("Algae Normal"));
 
     IS_L1
+        .or(IS_L2)
         .and(IS_REEF_MODE)
         .onTrue(
             Commands.runOnce(
@@ -441,8 +442,7 @@ public class RobotContainer {
                   driveCommand.setReefMode(DriveCommand.ReefPositions.BACK_REEF);
                 }));
 
-    IS_L2
-        .or(IS_L3)
+    IS_L3
         .or(IS_L4)
         .and(IS_REEF_MODE)
         .onTrue(
