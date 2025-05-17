8fc2ab418a5aee387e97bb357cac81f65c4585e9
Maggie Ferrer <MaFerrer2@s.sfusd.edu>
2025-05-16 22:52:33 -0700

    yay (#301)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -436,7 +436,6 @@ public class RobotContainer {
     //             .withName("Algae Normal"));
 
     IS_L1
-        .or(IS_L2)
         .and(IS_REEF_MODE)
         .onTrue(
             Commands.runOnce(
@@ -444,7 +443,8 @@ public class RobotContainer {
                   driveCommand.setReefMode(DriveCommand.ReefPositions.BACK_REEF);
                 }));
 
-    IS_L3
+    IS_L2
+        .or(IS_L3)
         .or(IS_L4)
         .and(IS_REEF_MODE)
         .onTrue(
