e91fdd5cd97548a5ac62d8ab431a6d0a87a8cf0c
Caden <Cachen15@s.sfusd.edu>
2025-07-27 09:23:20 -0700

    L2 faces backwards

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -436,6 +436,7 @@ public class RobotContainer {
     //             .withName("Algae Normal"));
 
     IS_L1
+        .or(IS_L2)
         .and(IS_REEF_MODE)
         .onTrue(
             Commands.runOnce(
@@ -443,8 +444,7 @@ public class RobotContainer {
                   driveCommand.setReefMode(DriveCommand.ReefPositions.BACK_REEF);
                 }));
 
-    IS_L2
-        .or(IS_L3)
+    IS_L3
         .or(IS_L4)
         .and(IS_REEF_MODE)
         .onTrue(
@@ -515,7 +515,7 @@ public class RobotContainer {
 
     IS_L1
         .and(m_driverController.b())
-        .whileTrue(alignToPose(() -> EagleUtil.getClosestRightReefBack(drivetrain.getPose(0.25))));
+        .whileTrue(alignToPose(() -> EagleUtil.getClosestL1Back(drivetrain.getPose(0.25))));
 
     m_operatorController.start().onTrue(elevator.homingCommand());
 
