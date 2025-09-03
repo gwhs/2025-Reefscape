f0d4a419c0fc968797a87acea0660e5a01b54754
bella <betan1@s.sfusd.edu>
2025-09-03 16:09:25 -0700

    version9/3/2025

--- src/main/java/frc/robot/RobotContainerExercise.java
+++ src/main/java/frc/robot/RobotContainerExercise.java
@@ -41,14 +41,16 @@ public class RobotContainerExercise {
 
     // TODO 1: press left bumper: set arm angle to 120
 
+    controller.leftBumper().onTrue(arm.setAngle(120.0));
 
     // TODO 2: press right bumper -> set arm angle to -90
 
+    controller.rightBumper().onTrue(arm.setAngle(90.0));
 
     // TODO 3: press start -> score L4 Coral
     // ↓↓↓↓↓↓↓↓↓↓ COMPLETE THE COMMAND COMPOSITION IN scoreL4Coral() METHOD BELOW FIRST ↓↓↓↓↓↓↓↓↓↓
 
-    
+    controller.start().onTrue(scoreL4Coral());
   }
 
   public void periodic() {
@@ -60,13 +62,13 @@ public class RobotContainerExercise {
    ********************/
   public Command scoreL4Coral() {
     return Commands.sequence(
-        // TODO: 
+        // TODO:
         // Command 1: In parallel: (Commands.parallel())
         //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
         //     Command 1b: rotate arm to ArmConstants.L4_PREP_POSITION
         // Command 2: Spin endeffector at EndEffectorConstants.VOLTAGE_L4
         // Command 3: Wait 0.05 seconds (Commands.waitSeconds())
-        // Command 4: In parallel: 
+        // Command 4: In parallel:
         //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
         //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
         //     Command 4c: spin endeffector at 0 volts
