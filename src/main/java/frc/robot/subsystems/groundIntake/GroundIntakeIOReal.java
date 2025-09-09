2c0adac331ee71253546f6605aa145c6efbf5f7e
Caden <Cachen15@s.sfusd.edu>
2025-09-08 20:34:31 -0700

    Update at 'Mon Sep 08 20:34:31 PDT 2025'

--- src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeIOReal.java
@@ -113,7 +113,7 @@ public class GroundIntakeIOReal implements GroundIntakeIO {
     motorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
     currentConfig.withStatorCurrentLimitEnable(true);
-    currentConfig.withStatorCurrentLimit(40);
+    currentConfig.withStatorCurrentLimit(80);
 
     status = StatusCode.StatusCodeNotInitialized;
     for (int i = 0; i < 5; i++) {
