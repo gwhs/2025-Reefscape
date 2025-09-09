1201674cf3371eb123f7d63f88458a5d9e53521f
bella <betan1@s.sfusd.edu>
2025-09-08 17:26:18 -0700

    version 9/8/2025

--- src/main/java/frc/robot/RobotContainerExercise.java
+++ src/main/java/frc/robot/RobotContainerExercise.java
@@ -7,8 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.endEffector.EndEffectorConstants;
 import frc.robot.subsystems.endEffector.EndEffectorSubsystem;
 import frc.robot.subsystems.groundIntake.GroundIntakeSubsystem;
 
@@ -45,7 +48,7 @@ public class RobotContainerExercise {
 
     // TODO 2: press right bumper -> set arm angle to -90
 
-    controller.rightBumper().onTrue(arm.setAngle(90.0));
+    controller.rightBumper().onTrue(arm.setAngle(-90.0));
 
     // TODO 3: press start -> score L4 Coral
     // ↓↓↓↓↓↓↓↓↓↓ COMPLETE THE COMMAND COMPOSITION IN scoreL4Coral() METHOD BELOW FIRST ↓↓↓↓↓↓↓↓↓↓
@@ -62,6 +65,9 @@ public class RobotContainerExercise {
    ********************/
   public Command scoreL4Coral() {
     return Commands.sequence(
+        Commands.parallel(
+            elevator.setHeight(ElevatorConstants.L4_PREP_POSITION),
+            arm.setAngle(ArmConstants.L4_PREP_POSITION)),
         // TODO:
         // Command 1: In parallel: (Commands.parallel())
         //     Command 1a: extend elevator to ElevatorConstants.L4_PREP_POSITION
@@ -72,6 +78,12 @@ public class RobotContainerExercise {
         //     Command 4a: retract elevator to ElevatorConstants.STOW_METER
         //     Command 4b: rotate arm to ArmConstants.ARM_STOW_ANGLE
         //     Command 4c: spin endeffector at 0 volts
-        );
+
+        endEffector.setVoltage(EndEffectorConstants.VOLTAGE_L4),
+        Commands.waitSeconds(0.05),
+        Commands.parallel(
+            elevator.setHeight(ElevatorConstants.STOW_METER),
+            arm.setAngle(ArmConstants.ARM_STOW_ANGLE),
+            endEffector.setVoltage(0)));
   }
 }
