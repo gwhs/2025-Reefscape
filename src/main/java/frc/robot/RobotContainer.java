54480981731c997b9ecdb0da13726c617dc7dd31
Emily Yu <EmilyYYu@hotmail.com>
2025-09-17 15:37:11 -0700

    Encoders Added (#314)
    * Encoders Added
    
    * Update at 'Mon Sep 08 20:01:19 PDT 2025'
    
    * Update at 'Mon Sep 08 20:09:15 PDT 2025'
    
    * Update at 'Mon Sep 08 20:13:04 PDT 2025'
    
    * Update at 'Mon Sep 08 20:20:28 PDT 2025'
    
    * Update at 'Mon Sep 08 20:21:14 PDT 2025'
    
    * Update at 'Mon Sep 08 20:26:14 PDT 2025'
    
    * Update at 'Mon Sep 08 20:27:51 PDT 2025'
    
    * Update at 'Mon Sep 08 20:29:44 PDT 2025'
    
    * Update at 'Mon Sep 08 20:34:31 PDT 2025'
    
    * changed some constants
    
    * Update at 'Wed Sep 10 16:20:23 PDT 2025'
    
    * Update at 'Wed Sep 10 16:22:23 PDT 2025'
    
    * finished tuning constants
    
    * Update at 'Wed Sep 10 16:45:50 PDT 2025'
    
    * Update at 'Wed Sep 10 16:47:24 PDT 2025'
    
    * .
    
    * Update at 'Wed Sep 10 17:14:16 PDT 2025'
    
    * Update at 'Wed Sep 10 17:15:15 PDT 2025'
    
    * Update at 'Wed Sep 10 17:15:53 PDT 2025'
    
    * Update at 'Wed Sep 10 17:19:33 PDT 2025'
    
    * Update at 'Wed Sep 10 17:26:50 PDT 2025'
    
    * Update at 'Wed Sep 10 17:29:07 PDT 2025'
    
    * Update at 'Wed Sep 10 17:34:21 PDT 2025'
    
    * Update at 'Wed Sep 10 17:35:59 PDT 2025'
    
    * Update at 'Wed Sep 10 17:37:09 PDT 2025'
    
    * Update at 'Wed Sep 10 17:40:40 PDT 2025'
    
    * Update at 'Wed Sep 10 17:42:07 PDT 2025'
    
    * Update at 'Wed Sep 10 17:46:41 PDT 2025'
    
    * Update at 'Wed Sep 10 17:48:21 PDT 2025'
    
    * Update at 'Wed Sep 10 17:49:26 PDT 2025'
    
    * Update at 'Wed Sep 10 17:51:13 PDT 2025'
    
    * number changes
    
    * .
    
    * Update at 'Mon Sep 15 16:31:42 PDT 2025'
    
    * Update at 'Mon Sep 15 16:33:28 PDT 2025'
    
    * Update at 'Mon Sep 15 16:38:35 PDT 2025'
    
    * Update at 'Mon Sep 15 16:38:55 PDT 2025'
    
    * Update at 'Mon Sep 15 16:45:09 PDT 2025'
    
    * additions
    
    * push
    
    * slowmode
    
    * isaac changes
    
    ---------
    
    Co-authored-by: Caden <Cachen15@s.sfusd.edu>
    Co-authored-by: Caton Q <caton868@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -378,6 +378,14 @@ public class RobotContainer {
 
     m_driverController.leftTrigger().onFalse(dealgae());
 
+    m_driverController
+        .leftBumper()
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  driveCommand.setTargetMode(DriveCommand.TargetMode.NORMAL);
+                }));
+
     IS_L4
         .and(m_driverController.y())
         .whileTrue(prepScoreCoral(CoralLevel.L4))
@@ -403,7 +411,7 @@ public class RobotContainer {
                 }));
 
     m_driverController
-        .back()
+        .rightBumper()
         .whileTrue(
             Commands.startEnd(
                     () -> {
