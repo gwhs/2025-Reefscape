54a128b811048a5ae0d3eb6d2ebbc27544b8db8a
Caton Q <caton868@gmail.com>
2025-09-20 10:59:58 -0700

    Update at 'Sat Sep 20 10:59:58 PDT 2025'

--- src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeConstants.java
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeConstants.java
@@ -24,7 +24,7 @@ public class GroundIntakeConstants {
 
   public static final double INTAKE_CORAL_AMP = 60;
   public static final double INTAKE_CORAL_DUTYCYCLE = 1;
-  public static final double HOLD_CORAL_AMP = 45;
+  public static final double HOLD_CORAL_AMP = 40;
   public static final double HOLD_CORAL_DUTYCYCLE = 0.1;
   public static final double SCORE_CORAL_AMP = -80;
   public static final double SCORE_CORAL_DUTYCYCLE = 0.9;
