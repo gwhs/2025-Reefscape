d0598b1bc9129332767ed8d3c0f2c3a94406615f
Caton Q <caton868@gmail.com>
2025-09-20 10:36:30 -0700

    tested

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.List;
 
 public class EagleUtil {
   protected static ArrayList<Pose2d> m_redPoses;
