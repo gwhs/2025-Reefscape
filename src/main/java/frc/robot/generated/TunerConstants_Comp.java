6f57cd1cac3a94d71f84b2f2963c54fe6af4d3c3
Caton Q <caton868@gmail.com>
2025-09-27 13:50:01 -0700

    Update at 'Sat Sep 27 13:50:01 PDT 2025'

--- src/main/java/frc/robot/generated/TunerConstants_Comp.java
+++ src/main/java/frc/robot/generated/TunerConstants_Comp.java
@@ -155,7 +155,7 @@ public class TunerConstants_Comp {
   private static final int kBackLeftDriveMotorId = 5;
   private static final int kBackLeftSteerMotorId = 6;
   private static final int kBackLeftEncoderId = 3;
-  private static final Angle kBackLeftEncoderOffset = Rotations.of(-0.204345703125);
+  private static final Angle kBackLeftEncoderOffset = Rotations.of(0.263671875);
   private static final boolean kBackLeftSteerMotorInverted = true;
   private static final boolean kBackLeftEncoderInverted = false;
 
