5d5f20ba2d6805ea3f8621f75315ebdb52fc9058
Caton Q <caton868@gmail.com>
2025-09-27 17:38:05 -0700

    Update at 'Sat Sep 27 17:38:05 PDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -607,8 +607,6 @@ public class RobotContainer {
         "Five_Cycle_Processor", new FiveCycle(this, groundIntake, climb, false));
     autoChooser.addOption(
         "Five_Cycle_Non_Processor", new FiveCycle(this, groundIntake, climb, true));
-    autoChooser.addOption(
-        "Choreo_Five_Cycle_Non_Processor", new ChoreoFiveCycle(this, groundIntake, climb, true));
     autoChooser.addOption("Push_One_Cycle", new PushOneCycle(this, groundIntake, climb));
     autoChooser.addOption(
         "Wheel_Radius_Chracterizaton",
