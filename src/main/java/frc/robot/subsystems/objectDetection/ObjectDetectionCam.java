0ccadb328de4ff0d99ed6195707f02625364a0df
Caden740 <Cachen448@gmail.com>
2025-10-06 16:47:14 -0700

    h

--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionCam.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionCam.java
@@ -64,9 +64,9 @@ public class ObjectDetectionCam {
     simTargetPose =
         new Pose3d(14, 6, 0, new Rotation3d(0, 0, Math.PI)); // placeholder, change later
     visionTarget = new VisionTargetSim(simTargetPose, simTargetModel);
+    visionSim.addVisionTargets(visionTarget);
     // second piece maybe
 
-    visionSim.addVisionTargets(visionTarget);
     cameraProp = new SimCameraProperties();
     cameraProp.setCalibration(1280, 800, Rotation2d.fromDegrees(90));
     cameraProp.setFPS(100);
