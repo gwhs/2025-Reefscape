6c61a375c6f25b64e2160d2f7416abe1838efd5c
Caden740 <Cachen448@gmail.com>
2025-11-19 16:06:49 -0800

    Update at 'Wed Nov 19 16:06:49 PST 2025'

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -276,7 +276,7 @@ public class DriveCommand extends Command {
 
       double errorY = coralRelativeToRobot.getY();
 
-      double kP = 0.55; // Change
+      double kP = 0.5507; // Change
 
       ChassisSpeeds assistedVectorRobotOriented = new ChassisSpeeds(0, errorY * kP, 0);
       DogLog.log("Intake Drive Assist/Assisted Robot Relative Vector", assistedVectorRobotOriented);
