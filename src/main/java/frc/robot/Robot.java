8a1e1d33f7e5268fcc6c0f14754fb3292f6144ee
Isaac Ng <isaacngkh@gmail.com>
2026-01-20 21:16:31 -0800

    no build constant

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,19 +37,6 @@ public class Robot extends TimedRobot {
     LiveWindow.disableAllTelemetry();
 
     DriverStation.silenceJoystickConnectionWarning(true);
-
-    NetworkTableInstance.getDefault()
-        .getStringTopic("/Metadata/Branch")
-        .publish()
-        .set(BuildConstants.GIT_BRANCH);
-    NetworkTableInstance.getDefault()
-        .getStringTopic("/Metadata/SHA")
-        .publish()
-        .set(BuildConstants.GIT_SHA);
-    NetworkTableInstance.getDefault()
-        .getStringTopic("/Metadata/DIRTY")
-        .publish()
-        .set("" + BuildConstants.DIRTY);
   }
 
   @Override
